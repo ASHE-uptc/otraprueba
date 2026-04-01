a8030e494b33e23c7768feefb75f7a76a980645d
internetashe-del <internetashe@gmail.com>
2026-04-01 18:21:10 -0500

    COMMIT ACEPTADO

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     //AXEL RINCON
-
+    //COMMIT ACEPTADO POR EL ADMIN
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
